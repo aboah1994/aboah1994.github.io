2e5769a25bc9098a7e204982c37f178c897d00c8
Owusu-Ansah Solomon <owusuansahsolomon39@gmail.com>
2025-06-19 08:54:26 +0000

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  rewrites: async () => [
+    {
+      source: "/",
+      destination: "/home",
+    },
+  ],
 };
 
 export default nextConfig;
